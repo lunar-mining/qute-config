0c6032771031daf5a8d92bd13f4f84a6bc9c20ce
lunar-mining <ihateface@autistici.org>
2022-11-06 09:40:59 +0100

    change search engine

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config.load_autoconfig(False)
 config.set("colors.webpage.darkmode.enabled", True)
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
-c.url.default_page = "duckduckgo.com"
-c.url.start_pages = "duckduckgo.com"
+c.url.searchengines = {'DEFAULT': 'https://searx.org/?q={}'}
+c.url.default_page = "https://searx.org/"
+c.url.start_pages = "https://searx.org/"
 config.set('content.javascript.enabled', False)
